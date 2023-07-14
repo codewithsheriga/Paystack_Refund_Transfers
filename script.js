8030fbf62d564dc7cd0382fba0510bb2c0e752a1
Sherigabia <orionsavvy@gmail.com>
2023-07-14 21:24:37 +0000

    starter code

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const loadingScreen = document.getElementById('loadingScreen');
+
+function showLoadingScreen() {
+  loadingScreen.style.display = 'flex';
+}
+
+function hideLoadingScreen() {
+  loadingScreen.style.display = 'none';
+}
+
+
